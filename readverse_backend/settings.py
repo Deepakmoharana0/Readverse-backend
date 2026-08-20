52f46a4e92d5615faba6c11b25dc02cd5a847f77
Deepakmoharana0 <deepakmoharana38@gmail.com>
2026-08-20 14:18:59 +0530

    Fix CORS

--- readverse_backend/settings.py
+++ readverse_backend/settings.py
@@ -135,8 +135,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://readverse-store.netlify.app",
-    "https://readversestore.netlify.app",
+    "https://readverseapp.netlify.app",
 ]
 
 REST_FRAMEWORK = {
